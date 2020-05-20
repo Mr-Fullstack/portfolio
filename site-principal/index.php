50fc237b6d595bc6272e42f5483ef5e100f6427f
Marcos Antonio Cerqueira Souza de Freitas <marcosfullstack@gmail.com>
2020-05-20 14:28:42 -0300

    ajuste maior parte mobile

---
+++ site-principal/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/main.min.css">
+    <title>Mr fullstack</title>
+</head>
+<body>
+    <div class="background"></div>
+    <div class="container">
+        <div class="row">
+            <header class="col text-center">
+                <h1>Marcos Antonio Cerqueira </h1>
+                <h3>mrfullstack</h3>
+            </header>
+        </div>
+        <div class="row">
+            <main class="">
+                
+            </main>
+        </div>
+    </div>
+
+   
+    <script src="./bootstrap.min.js"></script>
+</body>
+</html>
